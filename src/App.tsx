0d45894df065e46f0355997119fe16a55b5ae4dc
Alexey Belyakovskiy <2697024@gmail.com>
2019-07-21 01:15:09 +0300

    Clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {upload} from 'apollo-uploader';
 
 class App extends React.Component {
@@ -8,19 +6,14 @@ class App extends React.Component {
     handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
             upload(e.target.files[0], {})
-                .then(() => console.log('Upload success'))
-                .catch((e: Error) => console.log('Error', e))
+                .subscribe((value: any) => console.log(value));
         }
     };
 
     render(): React.ReactElement | string {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-
-                    <input type="file" onChange={this.handleFileChange}/>
-                </header>
+            <div>
+                <input type="file" onChange={this.handleFileChange}/>
             </div>
         );
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import {ApolloClient} from "apollo-client";
 import {createUploadLink, Uploader} from "apollo-uploader";
@@ -10,22 +8,8 @@ import {InMemoryCache} from "apollo-cache-inmemory";
 
 const apolloClient =  new ApolloClient({
     link: createUploadLink('http://localhost'), // concat with your other links (i.e. http, error etc.)
-    cache: new InMemoryCache({}),
+    cache: new InMemoryCache({})
 });
-
-Uploader.init(apolloClient, (id: string, fileName: string): Promise<any> => {
-    return new Promise((resolve: (id: string) => void) => {
-
-        console.log(id, fileName);
-        // ...do some stuff after upload is success
-
-        resolve(id)
-    });
-});
+Uploader.init(apolloClient);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
