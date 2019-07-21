1d19687d0e70b7322b91dea63caac02eacb9ff52
Alexey Belyakovskiy <2697024@gmail.com>
2019-07-21 23:24:45 +0300

    Display process list

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,65 @@
 import * as React from 'react';
-import {upload} from 'apollo-uploader';
+import {upload, Queries} from 'apollo-uploader';
+import {Query} from "react-apollo";
+import {FileUploadProcess} from "apollo-uploader/lib/types";
+import './App.css'
 
 class App extends React.Component {
 
     handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-            upload(e.target.files[0], {})
-                .subscribe((value: any) => console.log(value));
+        const {files} = e.target;
+
+        if (files) {
+            Array.from(files).forEach(file => {
+                upload(file).subscribe((process: FileUploadProcess) => console.log(process));
+            });
         }
     };
 
     render(): React.ReactElement | string {
         return (
             <div>
-                <input type="file" onChange={this.handleFileChange}/>
+                <h2>Choose file</h2>
+                <input type="file" onChange={this.handleFileChange} multiple={true}/>
+
+                <h2>Upload processes</h2>
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>id</th>
+                        <th>fileName</th>
+                        <th>status</th>
+                        <th>fileSize</th>
+                        <th>loaded</th>
+                        <th>total</th>
+                        <th>result</th>
+                        <th>error</th>
+                    </tr>
+
+                    <Query
+                        query={Queries.uploading}
+                    >
+                        {({data}: { data: { uploading: FileUploadProcess[] } }) =>
+                            data.uploading.map((process: FileUploadProcess) => {
+                                const result = JSON.parse(process.result);
+
+                                return (
+                                    <tr key={process.id}>
+                                        <td> {process.id} </td>
+                                        <td> {process.fileName} </td>
+                                        <td> {process.status} </td>
+                                        <td> {process.loaded} </td>
+                                        <td> {process.fileSize} </td>
+                                        <td> {process.total} </td>
+                                        <td> {result.id} </td>
+                                        <td> {process.error} </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </Query>
+                    </tbody>
+                </table>
             </div>
         );
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import {ApolloProvider} from 'react-apollo';
 import {ApolloClient} from "apollo-client";
 import {createUploadLink, Uploader} from "apollo-uploader";
 import {InMemoryCache} from "apollo-cache-inmemory";
 
-const apolloClient =  new ApolloClient({
+import App from './App';
+
+const apolloClient = new ApolloClient({
     link: createUploadLink('http://localhost'), // concat with your other links (i.e. http, error etc.)
     cache: new InMemoryCache({})
 });
 Uploader.init(apolloClient);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ApolloProvider client={apolloClient}>
+        <App/>
+    </ApolloProvider>, document.getElementById('root'));
